35e259fa6e9e4164c61932382e1ee22510fe1ed6
fchevalier2017 <fchevalier2017@12-UC29-22.ad.campus-eni.fr>
2018-01-30 16:18:49 +0100

    BLLException complet

--- ResaConcert/src/fr/eni/ResaConcert/bll/BLLException.java
+++ ResaConcert/src/fr/eni/ResaConcert/bll/BLLException.java
@@ -1,5 +1,23 @@
 package fr.eni.ResaConcert.bll;
 
-public class BLLException {
+public class BLLException extends Exception {
+	public BLLException() {
+		super();
+	}
+	
+	public BLLException(String message) {
+		super(message);
+	}
+	
+	public BLLException(String message, Throwable exception) {
+		super(message, exception);
+	}
 
+	@Override
+	public String getMessage() {
+		
+		return "BLL - " + super.getMessage();
+	}
+	
+	
 }
