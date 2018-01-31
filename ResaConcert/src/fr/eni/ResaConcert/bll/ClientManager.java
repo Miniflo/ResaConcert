307817cea19eeca88a95080c85e27b3e7b89c444
fchevalier2017 <fchevalier2017@12-UC29-22.ad.campus-eni.fr>
2018-01-31 17:23:09 +0100

    Client Manager presque complet

---
+++ ResaConcert/src/fr/eni/ResaConcert/bll/ClientManager.java
@@ -0,0 +1,73 @@
+package fr.eni.ResaConcert.bll;
+
+
+import fr.eni.ResaConcert.dal.*;
+import fr.eni.ResaConcert.bo.*;
+import java.util.List;
+
+public class ClientManager {
+
+	private static ClientDAO daoClient;
+	
+	public ClientManager() throws BLLException{
+		daoClient = DAOFactory.getClientDAO();
+	}
+	
+	
+	public List<Client> getClient() throws BLLException{
+		List<Client> clients = null;
+		try {
+			clients = daoClient.selectAll();
+		} catch (DALException e){
+			e.printStackTrace();
+			throw new BLLException("Erreur récupération Client", e);
+		}
+		return clients;
+	}
+	
+	public void addClient(Client newClient) throws BLLException {
+		if(newClient.getClass()!=null){
+			throw new BLLException("Article deja existant.");
+		}
+		try {
+			validerClient(newClient);
+			daoClient.insert(newClient);
+		} catch (DALException e){
+			throw new BLLException("Echec d'ajout du client.", e);
+		}
+	}
+	
+	public void validerClient(Client c) throws BLLException{
+		boolean valide = true;
+		StringBuffer sb = new StringBuffer();
+		
+		if(c.getvNom()==null || c.getvNom().trim().length()==0){
+			sb.append("Le nom est obligatoire.\n");
+			valide = false;
+	}
+		if(c.getvPrenom()==null || c.getvPrenom().trim().length()==0){
+			sb.append("Le prénom est obligatoir\n");
+			valide = false;
+	}
+		if(c.getvEmail()==null || c.getvEmail().trim().length()==0){
+			sb.append("L'Email est obligatoire.\n");
+			valide = false;
+	}
+		if(c.getvAdresse()==null || c.getvAdresse().trim().length()==0){
+			sb.append("L'Adresse est obligatoire.\n");
+			valide = false;
+		}
+		if(c.getvCode_postal()==null || c.getvCode_postal().trim().length()==0){
+			sb.append("Le code postal est obligatoire.\n");
+			valide = false;
+		}
+		if(c.getvVille()==null || c.getvVille().trim().length()==0){
+			sb.append("La ville est obligatoire.\n");
+			valide = false;
+		}
+		
+	if(!valide){
+		throw new BLLException(sb.toString());
+	}
+	}
+}
