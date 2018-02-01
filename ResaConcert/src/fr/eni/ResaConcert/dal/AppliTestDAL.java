babe031c849061a3fd62c12fe44090dbf4b19987
rdicheautornier2017 <rdicheautornier2017@12-UC29-11.ad.campus-eni.fr>
2018-02-01 14:51:13 +0100

    Salut

--- ResaConcert/src/fr/eni/ResaConcert/dal/AppliTestDAL.java
+++ ResaConcert/src/fr/eni/ResaConcert/dal/AppliTestDAL.java
@@ -7,6 +7,11 @@ import java.util.List;
 
 import fr.eni.ResaConcert.bo.Client;
 import fr.eni.ResaConcert.bo.Spectacle;
+import pn.ClientDAO;
+import pn.DAOFactory;
+import pn.Reservation;
+import pn.ReservationDAO;
+import pn.SpectacleDAO;
 
 
 public class AppliTestDAL {
@@ -15,13 +20,9 @@ public class AppliTestDAL {
 
 		ClientDAO clientDAO = DAOFactory.getClientDAO();
 		SpectacleDAO spectacleDAO = DAOFactory.getSpectacleDAO();
+		ReservationDAO reservationDAO = DAOFactory.getReservationDAO();
 
 
-
-
-		
-			//SÃ©lection de tous les clients
-			//TODO...
 			Client vClient = new Client("Chevalié","Floryan","flolebg@wanadoo.èfère","La Remaudiere", "60000","POuet");
 			
 			clientDAO.insert(vClient);
@@ -29,14 +30,26 @@ public class AppliTestDAL {
 			System.out.println("Selection de tous les clients  : " + clients.toString() );
 			Client client = clientDAO.selectById(1);
 			System.out.println("Selection du client  : " + client.toString() );
-			
 			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+
+			java.util.Date utilDate = simpleDateFormat.parse("2018/12/31");
+			java.sql.Date vdate = new java.sql.Date(utilDate.getTime());
 			
-			Date vdate = simpleDateFormat.parse("2018/12/31");
+			
 			
 			Spectacle vSpectacle = spectacleDAO.selectById(1);
 			System.out.println("Premier Spectacle : " + vSpectacle.toString() );
+						
+			Reservation vReservation = new Reservation( 1, 1, 3,vdate);
+			Reservation vReservation2 = new Reservation( 1, 1, 2,vdate);
 
+			reservationDAO.insert(vReservation);
+			reservationDAO.insert(vReservation2);
+			
+			List<Reservation> vReservationsRecupereesviaIDClient = reservationDAO.selectByClient(1);
+			System.out.println("\n\n");
+			System.out.println("Les reservations recuperees via idclient sont : "+vReservationsRecupereesviaIDClient.get(0)+vReservationsRecupereesviaIDClient.get(1));
+	
 	
 	}
 
